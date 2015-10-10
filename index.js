ff03a15ed35067e92570fcac880e405ba6cde83c
Ramon Victor <ramon.wd@gmail.com>
2015-10-10 19:38:36 +0200

    version 0.1.17: add data-* attribute support

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var gulpProtractorQA = {
 		var _this = this,
 			results,
 			regexList = _this.ptorFindElements.regex;
-		
+
 		// lopp regexlist
 		for( var i = 0; i<regexList.length; i++ ){
 			// verify matches
@@ -35,7 +35,7 @@ var gulpProtractorQA = {
 					attrName : regexList[i].attrName,
 					fileName : path
 				};
-				
+
 				_this.ptorFindElements.foundList.push( res );
 
 			}
@@ -77,12 +77,12 @@ var gulpProtractorQA = {
 
 		for( var i = 0; i < _this[ collection ].length; i++ ){
 			var obj = _this[ collection ][i];
-		    if( typeof obj.path != 'undefined' ){
-		       	if( filepath.indexOf( obj.path.replace("./", "/") ) >= 0 ){
-		       		obj.contents = newContent;
-		       		break;
-		       	}
-		    }
+			if( typeof obj.path != 'undefined' ){
+				if( filepath.indexOf( obj.path.replace("./", "/") ) >= 0 ){
+					obj.contents = newContent;
+					break;
+				}
+			}
 		}
 
 		// reset foundList and totalNumOfElements
@@ -103,7 +103,7 @@ var gulpProtractorQA = {
 				exists = true;
 			}
 		}
-		
+
 		return exists;
 	},
 
@@ -112,47 +112,49 @@ var gulpProtractorQA = {
 		var _this = this,
 			allElementsFound = true;
 
-		for( var i = 0; i<foundList.length; i++ ){
+		for( var i = 0; i < foundList.length; i++ ){
 
 			var found = false;
 			var foundItem = foundList[i];
 
 			for(var c = 0; c < _this.viewFiles.length; c++ ){
 				var obj = _this.viewFiles[c];
-				
+
 				$ = cheerio.load( obj.contents );
 				var selector = '';
 
 				if( foundItem.type === "attr" ){
-				 	selector = [ '[', foundItem.attrName, '="', foundItem.val, '"]' ].join("");
+					selector = [
+						'[', foundItem.attrName, '="', foundItem.val, '"], ',
+						'[data-', foundItem.attrName, '="', foundItem.val, '"]'
+					].join("");
 
-				 	if( foundItem.attrName === "ng-bind" ){
-				 		// search for {{bindings}}
-				 		if( _this.bindExists( foundItem.val, obj.contents ) ){
-				 			found = true;
-				 		}
-				 	}
-
+					if( foundItem.attrName === "ng-bind" ){
+						// search for {{bindings}}
+						if( _this.bindExists( foundItem.val, obj.contents ) ){
+							found = true;
+						}
+					}
 				} else if( foundItem.type === "cssAttr" ){
 					selector = [ '[', foundItem.val, ']' ].join("");
 				}
 
-				if( $( selector ).length ){
+				if ($(selector).length) {
 					found = true;
 				}
 			}
 
-			if( !found ){ 
+			if( !found ){
 				allElementsFound = false;
-				gutil.log('[' + chalk.cyan(PLUGIN_NAME) + '] ' + chalk.red(foundItem.at) + ' at ' + chalk.bold(foundItem.fileName)  + ' not found in view files!');
+				gutil.log('[' + chalk.cyan(PLUGIN_NAME) + '] ' + chalk.red(foundItem.at) + ' at ' + chalk.bold(foundItem.fileName)  + ' not found within view files!');
 			}
 
 		}
 
 		if( allElementsFound ){
-			gutil.log( 
-				'[' + chalk.cyan(PLUGIN_NAME) + '] ' + 
-				chalk.green("all test element found!") 
+			gutil.log(
+				'[' + chalk.cyan(PLUGIN_NAME) + '] ' +
+				chalk.green("all test elements were successfully found!")
 			);
 		}
 
@@ -164,9 +166,9 @@ var gulpProtractorQA = {
 
 		// watch test files changes
 		var gaze = new Gaze(src);
-		gaze.on('all', function(event, filepath) { 
+		gaze.on('all', function(event, filepath) {
 			fs.readFile(filepath, 'utf8', function(err, data){
-				if (err) { throw err };
+				if (err) { throw err; }
 				_this.updateFileContents(collection, filepath, data);
 			});
 		});
@@ -174,26 +176,26 @@ var gulpProtractorQA = {
 
 		// async map file contents
 		var async = function async(arg, callback) {
-		  	fs.readFile(arg, 'utf8', function (err, data) {
-			    if (err) { throw err };
-				callback(arg, data);
-			});
+		fs.readFile(arg, 'utf8', function (err, data) {
+			if (err) { throw err; }
+			callback(arg, data);
+		});
 		};
 
 		glob.glob(src, function (er, files) {
 				files.forEach(function(item) {
 					async(item, function(filePath, data){
-					    
-					    _this[ collection ].push(
+
+						_this[ collection ].push(
 							{
 								path : filePath,
 								contents : data
 							}
 						);
 
-					    if( _this[ collection ].length == files.length ) {
-					      _cb();
-					    }
+						if( _this[ collection ].length == files.length ) {
+							_cb();
+						}
 					});
 				});
 		});
@@ -201,7 +203,7 @@ var gulpProtractorQA = {
 	},
 
 	bindStoreFileContents : function( _callback ){
-		
+
 		var _this = this;
 
 		_this.storeFileContents( _this.options.testSrc, "testFiles", function(){
@@ -218,7 +220,7 @@ gulpProtractorQA.init = function( options ){
 	var globals = gulpProtractorQA;
 
 	globals.options = options || {};
-		
+
 	if (typeof globals.options.testSrc == 'undefined') {
 		throw new gutil.PluginError(PLUGIN_NAME, '`testSrc` required');
 	}
@@ -228,10 +230,10 @@ gulpProtractorQA.init = function( options ){
 
 	globals.bindStoreFileContents(function(){
 		globals.searchProtractorDotByContents(globals.testFiles, function beforeViewMatches(){
-			gutil.log( '[' + chalk.cyan(PLUGIN_NAME) + '] ' + chalk.gray( "// " + globals.ptorFindElements.foundList.length + " out of " + globals.totalNumOfElements + " element selectors are been watched" ) );
+			gutil.log( '[' + chalk.cyan(PLUGIN_NAME) + '] ' + chalk.gray( "// " + globals.ptorFindElements.foundList.length + " out of " + globals.totalNumOfElements + " element selectors are being watched" ) );
 		});
 	});
-	
+
 };
 
 // Exporting the plugin main function
